74a2be3a4e79a5127cc7e0f200f271f10192e337
Behtsa <monze_gdbm@hotmail.com>
2017-10-31 18:27:30 -0600

    cc code modified

--- credit-card-validation/js/app.js
+++ credit-card-validation/js/app.js
@@ -1,23 +1,11 @@
 var isValidCard = function(cc){
-	for (var i = 0; i >= 16; i++) {
-		if((i + 1) % 2 != 0){
-			var division = cc[ i + 1 ] * 2;
-			if(division > 9){
-				var sum = division[0] + division[1];
-				console.log(sum);
-			}
-		}
-
-	}
+	var arrayCc = Array.from(cc);
+	var arrayCcReversed = arrayCc.reverse();
+	return arrayCcReversed;
 }
 
-var creditCard = prompt("Enter your credit card number: ");
+var creditCard = prompt('Enter your Credit Card number');
 
-if(creditCard.length == 0){
-	alert("You must entered your credit card number");
-} else if(creditCard.length > 16) {
-	alert("Only 16 digits are valid");
-} else if(creditCard.length == 16){
-	var creditCardInt = parseInt(creditCard);
-	console.log(isValidCard(creditCardInt));
-}
+console.log(isValidCard(creditCard));
+
+
